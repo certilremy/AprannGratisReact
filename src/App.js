17c8c4c2bff42f0252d9e74222d5768a0fc213fb
certilremy <certilremy@gmail.com>
2019-04-11 15:12:42 -0400

    added reac-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 import Posts from '../src/components/posts/posts';
 
 class App extends Component {
   render () {
-    return <Posts />;
+    return (
+      <Container>
+        <Posts />
+      </Container>
+    );
   }
 }
 
